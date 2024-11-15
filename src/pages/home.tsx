3febdcdae59dedff279c85a04ffa491be15e6392
cletqui <antoine.quillivic@gmail.com>
2024-11-15 10:22:36 +0100

    fix

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -100,7 +100,7 @@ app.get("/", (c: Context) => {
 
 app.get("/robots.txt", (c: Context) => c.redirect("/static/robots.txt", 301));
 
-app.get("/keybase.txt", serveStatic({ path: "./static/keybase.txt" }));
+app.get("/keybase.txt", (c: Context) => c.redirect("/static/keybase.txt", 301));
 
 /* EASTER EGGS */
 app
