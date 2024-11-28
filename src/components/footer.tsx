c2971ba9c2dfd67af5aeac78cd8d6b3f587d3154
cletqui <antoine.quillivic@gmail.com>
2024-11-28 09:58:18 +0100

    fix Footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,37 +9,22 @@ const FooterIcon = ({ icon, href }: { icon: string; href: string }) => (
   </a>
 );
 
-const LoveAndCoffee = ({ lang }: { lang: string }) => {
-  return (
-    <div class="uk-flex uk-flex-middle">
-      {lang === "fr" ? "Fait avec" : "Made with"}
-      <FooterIcon icon="heart" href="https://beautifulhandwrittenletters.com" />
-      {lang === "fr" ? "et" : "and"}
-      <FooterIcon icon="coffee" href="https://www.buymeacoffee.com/cletqui" />
-    </div>
-  );
-};
-
-const Issues = ({ lang }: { lang: string }) => {
-  return (
-    <div class="uk-flex uk-flex-middle">
-      {lang === "fr" ? "Signale les bugs sur" : "Report bugs on"}
-      <FooterIcon
-        icon="github"
-        href="https://github.com/cletqui/portfolio/issues"
-      />
-    </div>
-  );
-};
-
 export const Footer = ({ lang }: { lang: string }) => (
   <div uk-sticky="position: bottom">
-    <div
-      class="uk-flex uk-flex-column uk-flex-middle uk-text-muted uk-padding-small uk-padding-small-bottom uk-padding-top" /* TODO use Grid */
-    >
-      <div class="uk-grid-divider uk-text-small" uk-grid>
-        <LoveAndCoffee lang={lang} />
-        <Issues lang={lang} />
+    <div class="uk-flex uk-flex-center uk-text-muted uk-padding-small-bottom uk-padding-top">
+      <div class="uk-flex uk-flex-middle">
+        {lang === "fr" ? "Fait avec" : "Made with"}
+        <FooterIcon
+          icon="heart"
+          href="https://beautifulhandwrittenletters.com"
+        />
+        {lang === "fr" ? "et" : "and"}
+        <FooterIcon icon="coffee" href="https://www.buymeacoffee.com/cletqui" />
+        {lang === "fr" ? "| Signale les bugs sur" : "| Report bugs on"}
+        <FooterIcon
+          icon="github"
+          href="https://github.com/cletqui/portfolio/issues"
+        />
       </div>
     </div>
   </div>
